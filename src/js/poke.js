50059175e1dfd510af2669d6320941c0ff189852
brenmiller27 <167664968+brenmiller27@users.noreply.github.com>
2025-05-23 18:11:57 -0700

    initial commit

---
+++ src/js/poke.js
@@ -0,0 +1,15 @@
+//fetch is used for making Http requests to fetch recourses
+//(json style data,images, files)
+//simplifies asynchronous data fetching in javascript
+//used for interating with APIs to retrieve and send data asynchrounusly over the web.
+// fetch(url,{options})
+
+
+fetchData();
+
+
+
+fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -28,3 +28,38 @@ function RenderListWithTemplate (templateFn, parentElement, list, position = "af
   this.listElement.insertAdjacentHTML('afterbegin', htmlStrings.join(''));
 
 }
+
+function RenderWithTemplate (template, parentElement, data, callback) {
+
+ parentElement.innerhtml = template;
+ if(callback) {
+  callback(data)
+ }
+ 
+}
+
+RenderListWithTemplate();
+RenderWithTemplate()
+
+export async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+
+  const headerTemplate = await loadTemplate("../partials/header.html");
+
+  const headerElement = document.querySelector("#main-header");
+
+  renderWithTemplate(headerTemplate, headerElement);
+
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(footerTemplate, footerElement);
+
+}
+
